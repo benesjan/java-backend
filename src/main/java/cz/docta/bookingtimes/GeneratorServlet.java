8c90a693bf283d40dd142a8bd96b06150dfccd08
Jan Beneš <janbenes1234@gmail.com>
2017-02-02 09:37:35 +0100

    Migration to flexible environment. Fully working Firebase communication

--- src/main/java/cz/docta/bookingtimes/GeneratorServlet.java
+++ src/main/java/cz/docta/bookingtimes/GeneratorServlet.java
@@ -1,17 +1,52 @@
 package cz.docta.bookingtimes;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredentials;
+import com.google.firebase.database.*;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 public class GeneratorServlet extends HttpServlet {
+
+    FirebaseDatabase database = null;
+
+    @Override
+    public void init(ServletConfig config) {
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredential(FirebaseCredentials.applicationDefault())
+                .setDatabaseUrl("https://doctor-appointment-system.firebaseio.com/")
+                .build();
+
+        FirebaseApp defaultApp = FirebaseApp.initializeApp(options);
+
+        this.database = FirebaseDatabase.getInstance(defaultApp);
+    }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter out = resp.getWriter();
-        out.println("TODO: generate book times");
+        out.println(this.database);
+        this.loadData();
+    }
+
+    private void loadData() {
+        DatabaseReference ref = this.database.getReference("appointmentsPublic");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
     }
 }

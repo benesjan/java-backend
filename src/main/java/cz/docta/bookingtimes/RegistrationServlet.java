119eb4fe100a4b3ecd4140df96e2351bdad56b16
Jan Beneš <janbenes1234@gmail.com>
2017-02-10 20:39:21 +0100

    CORS enabling headers

--- src/main/java/cz/docta/bookingtimes/RegistrationServlet.java
+++ src/main/java/cz/docta/bookingtimes/RegistrationServlet.java
@@ -1,7 +1,10 @@
 package cz.docta.bookingtimes;
 
 import com.google.firebase.database.*;
+import com.google.gson.Gson;
+import cz.docta.bookingtimes.gsonrequests.RegistrationRequest;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -10,13 +13,34 @@ import java.util.stream.Collectors;
 public class RegistrationServlet extends BookingTimesServlet {
 
     @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    }
+
+    @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Gson gson = new Gson();
+
+        String postData = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        RegistrationRequest registrationRequest = gson.fromJson(postData, RegistrationRequest.class);
+
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Content-Type", "application/json");
+
+        if (registrationRequest != null && registrationRequest.getOfficeId() != null) {
+            resp.getWriter().append("{\"success\":true}");
+            this.handleFirebaseLogic(registrationRequest.getOfficeId());
+        } else {
+            System.err.println("Request parsing failed.");
+            resp.getWriter().append("{\"success\":false}");
+        }
+    }
+
+    private void handleFirebaseLogic(String officeId) {
         FirebaseDatabase database = this.database;
-
-        String officeId = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-
-        resp.getWriter().println("OK");
-
         DatabaseReference ref = database.getReference("generatorInfo/" + officeId);
 
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -30,6 +54,5 @@ public class RegistrationServlet extends BookingTimesServlet {
                 System.err.println("The read failed: " + databaseError.getCode());
             }
         });
-
     }
 }
---
+++ src/main/java/cz/docta/bookingtimes/gsonrequests/RegistrationRequest.java
@@ -0,0 +1,16 @@
+package cz.docta.bookingtimes.gsonrequests;
+
+/**
+ * @author Jan Benes
+ */
+public class RegistrationRequest {
+    private String officeId;
+
+    public String getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(String officeId) {
+        this.officeId = officeId;
+    }
+}

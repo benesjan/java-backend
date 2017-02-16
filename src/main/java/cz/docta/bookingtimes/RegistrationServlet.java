e7dd03a1e68c85c84ebb7f96ef49d9b6dbcc6b31
Jan Beneš <janbenes1234@gmail.com>
2017-02-16 21:22:07 +0100

    Improved security of RegistrationServlet

--- src/main/java/cz/docta/bookingtimes/RegistrationServlet.java
+++ src/main/java/cz/docta/bookingtimes/RegistrationServlet.java
@@ -1,5 +1,6 @@
 package cz.docta.bookingtimes;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
 import com.google.gson.Gson;
 import cz.docta.bookingtimes.abstractpackage.FirebaseServlet;
@@ -32,18 +33,23 @@ public class RegistrationServlet extends FirebaseServlet {
         resp.addHeader("Access-Control-Allow-Origin", "*");
         resp.addHeader("Content-Type", "application/json");
 
-        if (registrationRequest != null && registrationRequest.getOfficeId() != null) {
+        if (registrationRequest != null && registrationRequest.areAttributesValid()) {
             resp.getWriter().append("{\"success\":true}");
-            this.handleFirebaseLogic(registrationRequest.getOfficeId());
+            this.verifyTokenAndGenerate(registrationRequest);
         } else {
             System.err.println("Request parsing failed.");
             resp.getWriter().append("{\"success\":false}");
         }
     }
 
-    private void handleFirebaseLogic(String officeId) {
-        FirebaseDatabase database = Generator.getServiceDatabase();
-        DatabaseReference ref = database.getReference("generatorInfo/" + officeId);
+    private void verifyTokenAndGenerate(RegistrationRequest registrationRequest) {
+        FirebaseAuth.getInstance(Generator.getServiceApp()).verifyIdToken(registrationRequest.getIdToken())
+                .addOnSuccessListener(decodedToken -> this.generateTimes(Generator.getUserDatabase(decodedToken.getUid()), registrationRequest))
+                .addOnFailureListener(e -> System.err.println("Token verification failed. RegistrationRequest: " + registrationRequest));
+    }
+
+    private void generateTimes(FirebaseDatabase database, RegistrationRequest registrationRequest) {
+        DatabaseReference ref = database.getReference("generatorInfo/" + registrationRequest.getOfficeId());
 
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -53,7 +59,7 @@ public class RegistrationServlet extends FirebaseServlet {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                System.err.println("Registration request - officeId: " + officeId + ", The read failed: " + databaseError.getCode());
+                System.err.println("Registration request - officeId: " + registrationRequest.getOfficeId() + ", The read failed: " + databaseError.getCode());
             }
         });
     }
--- src/main/java/cz/docta/bookingtimes/gsonrequests/RegistrationRequest.java
+++ src/main/java/cz/docta/bookingtimes/gsonrequests/RegistrationRequest.java
@@ -5,6 +5,11 @@ package cz.docta.bookingtimes.gsonrequests;
  */
 public class RegistrationRequest {
     private String officeId;
+    private String idToken;
+
+    public Boolean areAttributesValid() {
+        return this.officeId != null && this.idToken != null;
+    }
 
     public String getOfficeId() {
         return officeId;
@@ -12,5 +17,21 @@ public class RegistrationRequest {
 
     public void setOfficeId(String officeId) {
         this.officeId = officeId;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistrationRequest{" +
+                "officeId='" + officeId + '\'' +
+                ", idToken='" + idToken + '\'' +
+                '}';
     }
 }

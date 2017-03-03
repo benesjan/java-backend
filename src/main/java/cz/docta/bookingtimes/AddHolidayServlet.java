eaf02e5f97d4dd374eaf9c4c8dfb506568a836d0
Jan Beneš <janbenes1234@gmail.com>
2017-03-03 23:10:50 +0100

    AddHolidayServlet finished

--- src/main/java/cz/docta/bookingtimes/AddHolidayServlet.java
+++ src/main/java/cz/docta/bookingtimes/AddHolidayServlet.java
@@ -3,7 +3,6 @@ package cz.docta.bookingtimes;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
 import com.google.gson.Gson;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import cz.docta.bookingtimes.abstractpackage.FirebaseServlet;
 import cz.docta.bookingtimes.generator.Generator;
 import cz.docta.bookingtimes.gsonrequests.AddHolidayRequest;
@@ -88,18 +87,80 @@ public class AddHolidayServlet extends FirebaseServlet {
         String officeId = addHolidayRequest.getOfficeId();
         Integer visitLength = generatorSnapshot.child("visitLength").getValue(Integer.class);
 
+        // Slightly hacky
+        final Map<String, Boolean> synchronizedMap = new HashMap<>();
+
+        synchronizedMap.put("privateFinished", false);
+        synchronizedMap.put("publicFinished", false);
+
         // I have top remove all times that interfere with holiday so I am subtracting 1 visit length in milliseconds
         String startId = Generator.timestampToId(addHolidayRequest.getStartAt() - visitLength * 60 * 1000);
         String endId = Generator.timestampToId(addHolidayRequest.getEndAt());
 
-        // Might rekt a patient if he managed to create an appointment just before the appointments are deleted
+        // Might rekt a patient if he managed to create an appointment just before the appointments are deleted> possible handled automatically by Firebase
         database.getReference("appointmentsPublic/" + officeId).orderByKey().startAt(startId).endAt(endId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot time : dataSnapshot.getChildren()) {
                     objectToSave.put("/appointmentsPublic/" + officeId + "/" + time.getKey(), null);
                 }
-                saveObject(database, objectToSave, officeId);
+
+                synchronized (synchronizedMap) {
+                    if (synchronizedMap.get("privateFinished")) {
+                        saveObject(database, objectToSave, officeId);
+                    } else {
+                        synchronizedMap.put("publicFinished", true);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.err.println("AddHolidays request (deleteInterferingTimesAndSave method) - officeId: " + officeId + ", The read failed: " + databaseError.getCode());
+            }
+        });
+
+        database.getReference("appointmentsPrivate/" + officeId).orderByKey().startAt(startId).endAt(endId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, String> patientMessage = new HashMap<>();
+                patientMessage.put("message", "Vaše objednání bylo zrušeno kvůli dovolené. Děkujeme za pochopení");
+                patientMessage.put("title", "Zrušené objednání");
+                patientMessage.put("sender", officeId);
+
+                for (DataSnapshot time : dataSnapshot.getChildren()) {
+                    String patientId = time.child("patient").getValue(String.class);
+                    objectToSave.put("/appointmentsPrivate/" + officeId + "/" + time.getKey(), null);
+                    objectToSave.put("/userAppointments/" + patientId + "/" + time.getKey(), null);
+                    objectToSave.put("/userNotifications/" + patientId + "/" + database.getReference("/").push().getKey(), patientMessage);
+
+                    database.getReference("/userInterfaceInfo/" + patientId + "/numberOfNotifications").runTransaction(new Transaction.Handler() {
+                        @Override
+                        public Transaction.Result doTransaction(MutableData mutableData) {
+                            Integer currentValue = mutableData.getValue(Integer.class);
+                            if (currentValue == null) {
+                                mutableData.setValue(1);
+                            } else {
+                                mutableData.setValue(currentValue + 1);
+                            }
+
+                            return Transaction.success(mutableData);
+                        }
+
+                        @Override
+                        public void onComplete(DatabaseError databaseError, boolean committed, DataSnapshot dataSnapshot) {
+                            System.out.println("Transaction on notifications of " + patientId + " completed");
+                        }
+                    });
+                }
+
+                synchronized (synchronizedMap) {
+                    if (synchronizedMap.get("publicFinished")) {
+                        saveObject(database, objectToSave, officeId);
+                    } else {
+                        synchronizedMap.put("privateFinished", true);
+                    }
+                }
             }
 
             @Override
@@ -132,7 +193,7 @@ public class AddHolidayServlet extends FirebaseServlet {
         return false;
     }
 
-    private void saveObject(FirebaseDatabase database, Map<String, Object> objectToSave, String officeId) {
+    private synchronized void saveObject(FirebaseDatabase database, Map<String, Object> objectToSave, String officeId) {
         database.getReference("/").updateChildren(objectToSave, (databaseError, databaseReference) -> {
             if (databaseError != null) {
                 System.err.println("OfficeId: " + officeId + ", Holiday saving failed " + databaseError.getMessage());
